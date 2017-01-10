f6e45bfc0df9a6555b5e57618c1439ec11a55e28
Austin Burk <apburk@sudomemo.net>
2017-01-10 01:13:28 -0500

    Renamed the post classifier tool to a .sh and tweaked user_prefs slightly to reduce score of Ham messages (triggered because of Japanese characters in From)

--- Interactive_Post_Classifier.sh
+++ Interactive_Post_Classifier.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 // Using the Hatena Haiku API, fetches recent posts and stores them in an email-like format.
@@ -24,7 +25,9 @@ EOD;
 	return $email;
 }
 
-$public_timeline = json_decode(file_get_contents("http://h.hatena.ne.jp/api/statuses/public_timeline.json") , true);
+echo "Fetching recent posts...if a post has already been classified, it will be skipped.\n";
+$public_timeline = json_decode(file_get_contents("http://h.hatena.ne.jp/api/statuses/public_timeline.json?count=200") , true);
+$post_count = count($public_timeline);
 
 foreach($public_timeline as $key => $entry) {
 	$filename = strtotime($entry["created_at"]) . "." . $entry["id"] . "." . "_sudofox.spam.filter.lightni.ng_.txt";
@@ -43,8 +46,12 @@ Post ID:	{$entry["id"]}
 Post Body:	{$entry["text"]}
 ================
 EOD;
-	echo $info . "\n\n";
-	$decision = "-";
+	echo exec("clear");
+
+	echo $info . "\n";
+	echo "Entry $key of $post_count\n";
+	$decision = "";
+	
 	while (!in_array($decision, array("y","n"))) {
 		$decision = readline("Is this spam? (y/n): ");
 	}
@@ -56,3 +63,7 @@ EOD;
 	$storePath = "samples/" . ($spam ? "sp" : "h") . "am/$filename";
 	file_put_contents($storePath, $emailBody);
 }
+
+echo exec("clear");
+
+echo "All done!\n";
