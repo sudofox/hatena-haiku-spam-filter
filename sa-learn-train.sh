21f3ce9e9da92ead33fe989853b47520b710e206
Austin Burk <apburk@sudomemo.net>
2017-01-27 20:06:16 -0500

    Updated user_prefs scoring, allowed bayes files to be added as a decent starting point. Tweaked sa-learn script to only catch txt files

--- sa-learn-train.sh
+++ sa-learn-train.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # SpamAssassin Bayesian Filter training on collected samples.
 
-sa-learn --spam samples/spam/*
-sa-learn --ham samples/ham/*
-
+sa-learn --spam samples/spam/*.txt
+sa-learn --ham samples/ham/*.txt
